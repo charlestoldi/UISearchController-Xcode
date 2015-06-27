a2a410cd36f74dbde6c0726a50302a4f930b107f
Vea Software <pj@veasoftware.com>
2015-06-27 13:44:22 -0600

    Initial Commit

---
+++ UISearchController Xcode 7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISearchController Xcode 7
+//
+//  Created by PJ Vea on 6/27/15.
+//  Copyright © 2015 Vea Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
